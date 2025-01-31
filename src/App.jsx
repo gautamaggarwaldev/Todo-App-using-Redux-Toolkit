a3adb9bacac59aec0de548bee82691a747456b41
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-01-31 23:24:19 +0530

    add code & Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
 
 function App() {
 
   return (
-    <>
-      Redux-Toolkit
-    </>
+    <div className='container mx-auto p-6 w-200'>
+      <div className='text-4xl text-center font-serif font-bold mb-4 underline'>Redux Toolkit - Todo App</div>
+      <AddTodo />
+      <TodoList />
+    </div>
   )
 }
 
